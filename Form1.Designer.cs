81731a93136acef4d8605d78a5f29d8654fe290c
oldfox123 <hu_pingshan@hotmail.com>
2020-09-04 14:50:34 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonPort = new System.Windows.Forms.Button();
+            this.textBox_BaudRate = new System.Windows.Forms.TextBox();
+            this.textBox_Send = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxRcv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.Teal;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM List:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baudrate:";
+            // 
+            // ButtonPort
+            // 
+            this.ButtonPort.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ButtonPort.Location = new System.Drawing.Point(14, 120);
+            this.ButtonPort.Name = "ButtonPort";
+            this.ButtonPort.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPort.TabIndex = 4;
+            this.ButtonPort.Text = "打开串口";
+            this.ButtonPort.UseVisualStyleBackColor = false;
+            this.ButtonPort.Click += new System.EventHandler(this.ButtonPort_Click);
+            // 
+            // textBox_BaudRate
+            // 
+            this.textBox_BaudRate.Location = new System.Drawing.Point(14, 72);
+            this.textBox_BaudRate.Name = "textBox_BaudRate";
+            this.textBox_BaudRate.Size = new System.Drawing.Size(100, 21);
+            this.textBox_BaudRate.TabIndex = 5;
+            // 
+            // textBox_Send
+            // 
+            this.textBox_Send.Location = new System.Drawing.Point(198, 331);
+            this.textBox_Send.Multiline = true;
+            this.textBox_Send.Name = "textBox_Send";
+            this.textBox_Send.Size = new System.Drawing.Size(488, 94);
+            this.textBox_Send.TabIndex = 6;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonSend.Location = new System.Drawing.Point(702, 366);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 7;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBoxRcv
+            // 
+            this.textBoxRcv.Location = new System.Drawing.Point(198, 22);
+            this.textBoxRcv.Multiline = true;
+            this.textBoxRcv.Name = "textBoxRcv";
+            this.textBoxRcv.Size = new System.Drawing.Size(488, 272);
+            this.textBoxRcv.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(196, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Received:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(196, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "To be send:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel2.ForeColor = System.Drawing.Color.Indigo;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(68, 17);
+            this.toolStripStatusLabel2.Text = "串口未打开";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxRcv);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBox_Send);
+            this.Controls.Add(this.textBox_BaudRate);
+            this.Controls.Add(this.ButtonPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Serial";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonPort;
+        private System.Windows.Forms.TextBox textBox_BaudRate;
+        private System.Windows.Forms.TextBox textBox_Send;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxRcv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+     public partial class Form1 : Form
+    {
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            textBoxRcv.Text += serialPort1.ReadExisting();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] portList = SerialPort.GetPortNames();
+            for (int i = 0; i < portList.Length; i++)
+            {
+                string name = portList[i];
+                comboBox1.Items.Add(name);
+            }
+            comboBox1.SelectedIndex = 0;
+            textBox_BaudRate.Text = Settings.Default.BaudRate;
+            ButtonPort.BackColor = Color.DarkBlue;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //send
+            if (textBox_Send.Text!=null)
+            {
+                serialPort1.Write(textBox_Send.Text);
+            }
+        }
+
+        private void ButtonPort_Click(object sender, EventArgs e)
+        {
+            //textBox_Send.Text = comboBox1.Text;
+            if (serialPort1.IsOpen == false)
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32(textBox_BaudRate.Text);
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.RtsEnable = true;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                try
+                {
+                    serialPort1.Open();
+
+                    // 打开属性变为关闭属性
+                    ButtonPort.Text = "关闭串口";
+                    ButtonPort.BackColor = Color.Red;
+                    toolStripStatusLabel2.Text = "串口已打开";
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("串口连接失败！\r\n可能原因：串口被占用", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                serialPort1.Close();   // 关闭串口
+
+                // 打开属性变为关闭属性
+                ButtonPort.Text = "打开串口";
+                ButtonPort.BackColor = Color.DarkBlue;
+                toolStripStatusLabel2.Text = "串口已关闭";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text= "时间:" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");  // 显示当前时间
+        }
+    }
+}
